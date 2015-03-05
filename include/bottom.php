e4226c87316847680488cbefafe3c066db64e913
Ricky Thomson <flytheflag@gmx.co.uk>
2015-03-05 17:27:37 +0000

    Update bottom.php

--- include/bottom.php
+++ include/bottom.php
@@ -2,7 +2,7 @@
 </div>
 </noscript>
 <script>
-document.write("<body><div class=\"sub\"><span class=\"large2\">Please disable javascript to continue</span><hr /><div>We highly reccomend that you <span style=\"color:#ff0000; font-weight:bold;\">STOP</span> browsing the contents of this server while javascript is enabled.</div><hr /><i>Thank you. ~psy</i></div></body>");
+document.write("<body><div class=\"sub\"><span class=\"large2\">Please disable javascript to continue</span><hr /><div>We highly reccomend that you <span style=\"color:#ff0000; font-weight:bold;\">STOP</span> browsing the contents of this server while javascript is enabled.</div><hr /><i>Thank you.</i></div></body>");
 </script>
 
 </body>
