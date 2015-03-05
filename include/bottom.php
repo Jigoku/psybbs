a066b9fcaf8ce0ff3b3fc8a9921187fb77694ca0
Ricky Thomson <flytheflag@gmx.co.uk>
2015-03-05 17:27:06 +0000

    Update bottom.php

--- include/bottom.php
+++ include/bottom.php
@@ -2,7 +2,7 @@
 </div>
 </noscript>
 <script>
-document.write("<body><div class=\"sub\"><span class=\"large2\">Please disable javascript to continue</span><hr /><div>We highly reccomend that you <span style=\"color:#ff0000; font-weight:bold;\">STOP</span> browsing the contents of this server while javascript is enabled. Javascript can leak your true location effectively making any anonymity software useless. In the event a mallicious entity managed to inject javascript into our pages, you would be creating an attack vector by enabling javascript. Please reconfigure your browser settings and change your habits. If you are unsure on how to do this, we'd reccomend you use the Tor Browser Bundle to view our site.</div><hr /><i>Thank you. ~psy</i></div></body>");
+document.write("<body><div class=\"sub\"><span class=\"large2\">Please disable javascript to continue</span><hr /><div>We highly reccomend that you <span style=\"color:#ff0000; font-weight:bold;\">STOP</span> browsing the contents of this server while javascript is enabled.</div><hr /><i>Thank you. ~psy</i></div></body>");
 </script>
 
 </body>
