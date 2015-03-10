85390376e5d2b190152fc02128d50b4a57c97774
Ricky Thomson <flytheflag@gmx.co.uk>
2015-03-10 00:52:25 +0000

    update config example

--- include/config-example.php
+++ include/config-example.php
@@ -1,36 +1,36 @@
 <?php
-	//admin account details
-	$default_admin_username = "psy"; 
-	$default_admin_password = "psy";
+/* admin account */
+$default_admin_username = "psy";
+$default_admin_password = "psy";
 
-	//psyBBS version
-	$version = "0.3";
+/* mysql settings */
+$mysql_host 	= "localhost";
+$mysql_user 	= "";
+$mysql_pass 	= "";
+$mysql_database = "psyBBS";
+$mysql_salt 	= "";
 
-	//mysql settings
-	$mysql_host 	= "localhost";
-	$mysql_user 	= "";
-	$mysql_pass 	= "";
-	$mysql_database = "psyBBS";
-	$mysql_salt 	= "";
+/* global settings */
+$version = "0.3";
 
-	//text logo for the header
-	$site_title = "<span class=\"bannertext\">Example</span><span class=\"banneremphasis\">Text</span>";
-	$site_name = "psyBBS";
-	$site_announce = "Announcement: Welcome to psyBBS!";
-	$site_quote = "psyBBS $version";
+$site_title = "<span class=\"bannertext\">Example</span><span class=\"banneremphasis\">Board</span>";
+$site_name = "psyBBS";
+$site_announce = "Announcement: Registrations now open";
+$site_quote = "psyBBS $version";
 
-	//theme settings
-	$theme = "mintphosphor";
-	$themeopts = "?fixed";
+/* theme settings */
+$theme = "mintphosphor";
+$themeopts = "?fixed";
 
-	//page sorting
-	$items_per_page = 10;
+/* page sorting */
+$items_per_page = 10;
 
-	//user level aliases
-	$userLevel = [
-		"banned" => "remnant",
-		"member" => "shadow",
-		"moderator" => "agent",
-		"admin" => "operator",
-	];
+/* user level aliases */
+$userLevel = [
+	"banned" => "remnant",
+	"member" => "shadow",
+	"moderator" => "agent",
+	"admin" => "operator",
+];
+
 ?>
